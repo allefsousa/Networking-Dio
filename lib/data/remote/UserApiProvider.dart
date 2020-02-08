b1e540495549bb6721bcc04828108e8ee3d5aa36
Allef Sousa <allefsousa_1@hotmail.com>
2020-02-07 22:24:24 -0300

    Configurando interceptor do dio

--- lib/data/remote/UserApiProvider.dart
+++ lib/data/remote/UserApiProvider.dart
@@ -1,9 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_dio/data/model/UserResponse.dart';
+import 'package:flutter_dio/utils/LoggingInterceptor.dart';
 
-class UserApiProvider{
+class UserApiProvider {
   final String _endpoint = "https://randomuser.me/api/";
-  final Dio _dio = Dio();
+  Dio _dio;
+
+  UserApiProvider() {
+    BaseOptions options =
+    BaseOptions(receiveTimeout: 5000, connectTimeout: 5000);
+    _dio = Dio(options);
+    _dio.interceptors.add(LoggingInterceptor());
+  }
 
   Future<UserResponse> getUser() async {
     try {
@@ -11,7 +19,39 @@ class UserApiProvider{
       return UserResponse.fromJson(response.data);
     } catch (error, stacktrace) {
       print("Exception occured: $error stackTrace: $stacktrace");
-      return UserResponse.withError("$error");
+      return UserResponse.withError(_handleError(error));
     }
+  }
+
+  String _handleError(Error error) {
+    String errorDescription = "";
+    if (error is DioError) {
+      DioError dioError = error as DioError;
+      switch (dioError.type) {
+        case DioErrorType.CANCEL:
+          errorDescription = "Request to API server was cancelled";
+          break;
+        case DioErrorType.CONNECT_TIMEOUT:
+          errorDescription = "Connection timeout with API server";
+          break;
+        case DioErrorType.DEFAULT:
+          errorDescription =
+          "Connection to API server failed due to internet connection";
+          break;
+        case DioErrorType.RECEIVE_TIMEOUT:
+          errorDescription = "Receive timeout in connection with API server";
+          break;
+        case DioErrorType.RESPONSE:
+          errorDescription =
+          "Received invalid status code: ${dioError.response.statusCode}";
+          break;
+        case DioErrorType.SEND_TIMEOUT:
+          errorDescription = "Send timeout in connection with API server";
+          break;
+      }
+    } else {
+      errorDescription = "Unexpected error occured";
+    }
+    return errorDescription;
   }
 }
